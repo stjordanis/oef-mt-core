e1cd027d822a446e22262e7e807fb580e1d66c07
Katie Lucas <katie.lucas@fetch.ai>
2019-05-10 14:59:29 +0100

    Made taskpool a threadable rather than threaded object. Demo now does broadcasts in threads.

--- basic_comms/src/cpp/Core.hpp
+++ basic_comms/src/cpp/Core.hpp
@@ -19,9 +19,5 @@ public:
 
   std::shared_ptr<tcp::acceptor> makeAcceptor(unsigned short int port);
 private:
-
   std::shared_ptr<boost::asio::io_context> context;
-  using ThreadP  = std::shared_ptr<std::thread>;
-  using Threads = std::vector<ThreadP>;
-  Threads threads;
 };
