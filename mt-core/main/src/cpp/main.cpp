b22b646db26750d03e7088fe3ac6984233fd7964
Katie Lucas <katie.lucas@fetch.ai>
2019-05-23 11:32:11 +0100

    add program options parser

--- mt-core/main/src/cpp/main.cpp
+++ mt-core/main/src/cpp/main.cpp
@@ -2,11 +2,24 @@
 
 #include "mt-core/main/src/cpp/MtCore.hpp"
 
+#include <boost/program_options.hpp>
+
+
 int main(int argc, char *argv[])
 {
   // parse args
 
   MtCore::args args;
+
+  boost::program_options::options_description desc{"Options"};
+  desc.add_options()
+    ("help,h", "Help screen")
+    ;
+  boost::program_options::variables_map vm;
+  store(parse_command_line(argc, argv, desc), vm);
+  notify(vm);
+
+  // copy from VM to args.
 
   MtCore myCore;
   myCore.run(args);
