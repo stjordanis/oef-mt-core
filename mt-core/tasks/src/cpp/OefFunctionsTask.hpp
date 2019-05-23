508faf53361a53130298f8769be52ec1386b5dac
Katie Lucas <katie.lucas@fetch.ai>
2019-05-23 10:56:57 +0100

    del unned code

--- mt-core/tasks/src/cpp/OefFunctionsTask.hpp
+++ mt-core/tasks/src/cpp/OefFunctionsTask.hpp
@@ -13,16 +13,9 @@ public:
   }
 protected:
 private:
-  OefFunctionsTask(const OefFunctionsTask &other) = delete; // { copy(other); }
-  OefFunctionsTask &operator=(const OefFunctionsTask &other) = delete; // { copy(other); return *this; }
-  bool operator==(const OefFunctionsTask &other) = delete; // const { return compare(other)==0; }
-  bool operator<(const OefFunctionsTask &other) = delete; // const { return compare(other)==-1; }
+  OefFunctionsTask(const OefFunctionsTask &other) = delete;
+  OefFunctionsTask &operator=(const OefFunctionsTask &other) = delete;
+  bool operator==(const OefFunctionsTask &other) = delete;
+  bool operator<(const OefFunctionsTask &other) = delete;
 
-  //bool operator!=(const OefFunctionsTask &other) const { return compare(other)!=0; }
-  //bool operator>(const OefFunctionsTask &other) const { return compare(other)==1; }
-  //bool operator<=(const OefFunctionsTask &other) const { return compare(other)!=1; }
-  //bool operator>=(const OefFunctionsTask &other) const { return compare(other)!=-1; }
 };
-
-//namespace std { template<> void swap(OefFunctionsTask& lhs, OefFunctionsTask& rhs) { lhs.swap(rhs); } }
-//std::ostream& operator<<(std::ostream& os, const OefFunctionsTask &output) {}
